ee5f463cb434c54678e1c42051c3226f77540770
Systena <Systena@DESKTOP-4D0DV3C>
2019-12-06 12:10:09 +0900

    PC返却のため

--- src/main/java/jp/co/systena/tigerscave/granbluefantasy/controller/GrabulueController.java
+++ src/main/java/jp/co/systena/tigerscave/granbluefantasy/controller/GrabulueController.java
@@ -3,6 +3,7 @@ package jp.co.systena.tigerscave.granbluefantasy.controller;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,16 +19,30 @@ public class GrabulueController {
   @Autowired
   ListJob listJob;
 
+  
+  @RequestMapping(value="/mypage", method = RequestMethod.GET)          // URLとのマッピング
+  public ModelAndView index(ModelAndView mav) {
+    // Viewに渡すデータを設定
+    // セッション情報から保存したデータを取得してメッセージを生成
+    JobForm JobForm = (JobForm) session.getAttribute("form");
+    session.removeAttribute("form");
+    if (JobForm != null) {
+      mav.addObject("message", JobForm.getUsername()+"を保存しました");
+    }
+    mav.addObject("JobForm", new JobForm());  // 新規クラスを設定
 
-  @RequestMapping(value = "/mypage", method = RequestMethod.GET)//初期表示用のGET
-  public ModelAndView mypage(ModelAndView mav) {
+    
 
-    mav.addObject("JobFome", new JobForm());// (2)
-      // 使用するビューを設定
-      mav.setViewName("GameView"); // (3)
-
-      return mav;
+    BindingResult bindingResult = (BindingResult) session.getAttribute("result");
+    if (bindingResult != null) {
+      mav.addObject("bindingResult", bindingResult);
+    }
+    // Viewのテンプレート名を設定
+    mav.setViewName("GameView");
+    return mav;
   }
+  
+
   //ここにあたらしいPOSTメソッドを書く
 
   @RequestMapping(value = "/mypage", method = RequestMethod.POST) // URLとのマッピング
@@ -36,14 +51,14 @@ public class GrabulueController {
     // 変数式
     // ・コントローラーからテンプレートに値を渡す
     // ・変数「msg」に値を設定
-    mav.addObject("msg", "コントローラーからテンプレートに値を渡す"); // (2)
+    mav.addObject("message", JobForm.getUsername());
 
     // 使用するビューを設定
     mav.setViewName("Command"); // (3)
 
     return mav;
   }
-  
+/*  
   //リザルト画面のGET
   @RequestMapping(value = "/result", method = RequestMethod.GET)//初期表示用のGET
   public ModelAndView result(ModelAndView mav) {
@@ -58,6 +73,6 @@ public class GrabulueController {
 
     return mav;
   }
- 
+ */
 
 }
